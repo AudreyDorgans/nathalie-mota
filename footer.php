9df7fdf69d4e3e94b2452dcd848807c1604c7690
 AudreyDorgans <audrey@audreys-imac.home>
2024-03-26 11:24:41 +0100

    finalisation css et responsive header et footer

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 
 ?>
 
-
+<div class="container-footer">
 	<footer class="NM-footer">
 		<?php
     			wp_nav_menu([
@@ -22,6 +22,7 @@
   		?>
 		<span class="droits-reserves"> Tous droits réservés </span>
 	</footer>
+</div>
 </div> <!-- #NM-page -->
 
 <?php wp_footer(); ?>
